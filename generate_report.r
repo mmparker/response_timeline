b57b667ffa0802382784effddfa6e33bd71eabea
Matt Parker <mattmparker@gmail.com>
2013-07-22 09:46:32 -0600

    Generate named reports and autoarchive zip files.

--- generate_report.r
+++ generate_report.r
@@ -10,11 +10,36 @@ library(knitr)
 library(markdown)
 
 
+# Identify the zip file containing the results
+datafile <- list.files(pattern = "*.zip")
+
+# Extract the collector list
+collectors <- read.csv(unz(datafile, file.path("CSV", "CollectorList.csv")))
+
+
+# Get the name of the first collector - if probably formatted, you should
+# be able to strip the strategic area abbreviation from it
+# Pattern: any non-space characters from the beginning until the first space
+strat_area <- gsub(x = collectors$Title,
+                   pattern = "(^\\S*)\\s.*",
+                   replacement = "\\1")[1]
+
+
+# Generate a name for the report output file
+report_name <- paste(strat_area, "_", Sys.Date(), ".html", sep = "")
+
+
 # Knit it
 knit("response_timeline.rmd")
 
 markdownToHTML(file = "response_timeline.md",
-               output = "response_timeline.html",
+               output = file.path("reports", report_name),
                stylesheet = file.path("..", "qi_report.css"))
 
+
+# Archive the data file
+file.rename(from = datafile, to = file.path("archived_data", datafile))
+     
+# Remove the residual .md file
+file.remove(list.files(pattern = "response_timeline.md"))
      
